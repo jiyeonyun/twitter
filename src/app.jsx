968391f9f434041fdf3a7bd6233b5a6602348f73
jiyeonyun <jiyeonyun43@gmail.com>
2022-04-11 19:06:29 +0900

    Router setup

--- src/app.jsx
+++
@@ -1,9 +0,0 @@
-import './app.css';
-
-function App() {
-  return (
-    <div></div>
-  );
-}
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './app';
 import reportWebVitals from './reportWebVitals';
 import firebase from './firebase';
+import App from './components/app';
 
 console.log(firebase);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
