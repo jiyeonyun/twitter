6a3f3ba2d2554544d2d556efc793858c1c755708
jiyeonyun <jiyeonyun43@gmail.com>
2022-04-11 18:44:15 +0900

    firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
 import reportWebVitals from './reportWebVitals';
+import firebase from './firebase';
+
+console.log(firebase);
 
 ReactDOM.render(
   <React.StrictMode>
