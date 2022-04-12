6fb09a0cc7652770500c9f2386736c6b5a5022c1
jiyeonyun <jiyeonyun43@gmail.com>
2022-04-12 13:36:26 +0900

    Log out

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { authService } from '../myBase';
+import { Navigate, useNavigate } from 'react-router-dom';
 
-const Profile = (props) => (
-            <h1>Profile</h1>
-    );
+const Profile = (props) => {
+    const navigate = useNavigate();
+    const onLogOutClick = ()=>{
+        authService.signOut();
+        navigate('/');
+    }
+            return(
+                <>
+                    <button onClick={onLogOutClick}>Log Out</button>
+                </>
+            );
+        };
 
 export default Profile;
